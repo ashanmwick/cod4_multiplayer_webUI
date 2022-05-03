e13684ae09ddbce405f544850dd0c09e8d6e6ac3
AShan <ashainduwara2018@gmail.com>
2022-05-03 15:50:13 +0000

    AJAX request primary state

--- hello.py
+++ hello.py
@@ -1,8 +1,7 @@
 from flask import Flask,send_from_directory,render_template,request
 import os
-from maths import get_factors
 
-app = Flask(__name__)
+app = Flask(__name__,static_url_path='',static_folder='static')
 
 @app.route('/favicon.ico')
 def favicon():
@@ -10,7 +9,7 @@ def favicon():
 
 @app.route('/')
 def home_page():
-    return render_template('index.html',text_string="HI")
+    return render_template('index.html',text_string="HI",web_map_list=map_list)
 
 @app.route('/',methods=['POST'])
 def submit():
@@ -18,6 +17,12 @@ def submit():
     
     return render_template('index.html',text_string_2="HU")
 
+@app.route('/kill_house',methods=['GET'])
+def map_name():
+    name='<img src="images/killhouse.webp" alt="Girl in a jacket">'
+    return name
+
 
 if __name__ == '__main__':
+    map_list=["Kill House","Vacant"]
     app.run(debug=True,host='0.0.0.0', port=80)
